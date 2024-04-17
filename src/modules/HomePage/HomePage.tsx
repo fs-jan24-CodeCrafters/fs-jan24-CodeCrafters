63b22964122341f22c9f59252275661ca72215d4
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-04-17 17:47:42 +0200

    added productsSlider logic

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,3 +1,15 @@
+import { Container } from '../Shared/Container';
+import { ProductsSlider } from '../Shared/ProductsSlider';
+import products from '../../../public/api/products.json';
+
 export const HomePage: React.FC = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <ProductsSlider
+        products={products}
+        sliderTitle="Brand new models"
+      ></ProductsSlider>{' '}
+      {/* added for demo */}
+    </Container>
+  );
 };
