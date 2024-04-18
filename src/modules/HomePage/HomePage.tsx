128887852d18018a12ee07a7de37b7898f9d8d65
HolyRaveOS <kitlaruk1995@gmail.com>
2024-04-18 14:52:31 +0300

    refactored header

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,14 +1,9 @@
 import { Container } from '../Shared/Container';
-import { ProductsSlider } from '../Shared/ProductsSlider';
-import products from '../../../public/api/products.json';
+
 export const HomePage: React.FC = () => {
   return (
     <Container>
-      <ProductsSlider
-        products={products}
-        sliderTitle="Brand new models"
-      ></ProductsSlider>{' '}
-      {/* added for demo */}
+      <div>HomePage</div>
     </Container>
   );
 };
