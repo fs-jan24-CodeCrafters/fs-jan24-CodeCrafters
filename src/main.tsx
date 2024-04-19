5638f800b2b71c7c495bfc61da28e1291b5b1fbc
HolyRaveOS <kitlaruk1995@gmail.com>
2024-04-19 21:30:26 +0300

    add types packeges to lodash fuction

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import { App } from './App';
 import './index.scss';
-import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
