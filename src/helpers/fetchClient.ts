a9facb3edaa00903f64a24137d7ccf1f4b1635a2
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-05-23 15:17:08 +0200

    backend link changed

--- src/helpers/fetchClient.ts
+++ src/helpers/fetchClient.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const BASE_URL = 'http://localhost:3005';
+const BASE_URL = 'https://fs-jan24-code-crafters-server.vercel.app';
 
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
